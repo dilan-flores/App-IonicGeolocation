611c66a4b49001d5670072d45104bd395611692c
dilan-flores <dilan.flores@epn.edu.ec>
2023-12-06 16:41:29 -0500

    Ubicación-firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { BaseRouteReuseStrategy, RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { NativeGeocoder } from '@ionic-native/native-geocoder/ngx';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { environment } from '../environments/environment';
+import firebase from 'firebase/compat/app';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule],
+  //providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    Geolocation,
+    InAppBrowser,
+    NativeGeocoder,
+    {
+      provide: RouteReuseStrategy,
+      useClass: IonicRouteStrategy
+    }
+
+  ],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import firebase from 'firebase/compat/app';
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +11,46 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  latitude: any = 0;
+  longitude: any = 0;
 
-  constructor() {}
+  constructor(
+    private geolocation: Geolocation,
+    private inAppBrowser: InAppBrowser,
+    private firestore: AngularFirestore
+  ) { }
+
+  options = {
+    timeout: 10000,
+    enableHighAccuracy: true,
+    maximumAge: 3600
+  };
+
+  getCurrentCoordinates() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.latitude = resp.coords.latitude;
+      this.longitude = resp.coords.longitude;
+  
+      // Guarda las coordenadas en Firestore sin vincularlas a un usuario específico
+      this.firestore.collection('ubicaciones').add({
+        latitude: this.latitude,
+        longitude: this.longitude,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      })
+      .then(() => {
+        console.log('Ubicación guardada en Firebase');
+      })
+      .catch((error) => {
+        console.error('Error al guardar ubicación en Firebase:', error);
+      });
+    }).catch((error) => {
+      console.log('Error, no se puede obtener tu ubicacion', error);
+    });
+  }
+  
+  openGoogleMaps() {
+    const googleMapsUrl = `https://www.google.com/maps?q=${this.latitude},${this.longitude}`;
+    this.inAppBrowser.create(googleMapsUrl, '_system');
+  }
 
 }
